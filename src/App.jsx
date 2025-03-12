076050568242d4fb1204b7cb49fb65776d514abf
taybaa <shaikhtayba3@gmail.com>
2025-03-12 21:21:20 +0530

    fuckedup ui not displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline ">
-    Hello world!
-  </h1>
+    <Sidebar />
   )
 }
 
